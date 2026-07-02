bbe2a895a02322c2bb31dc3201cd2162a40621bc
2made1ra <im2made1ra@gmail.com>
2026-07-03 01:09:40 +0500

    init env, create .gitignore, create TextNode - base class for part of text

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode("Some anchor text", TextType.LINK, "https://www.boot.dev")
+
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
