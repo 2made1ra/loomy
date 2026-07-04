9d73d588c59aa9ba98a6df26de7042bff4cf7d33
2made1ra <im2made1ra@gmail.com>
2026-07-05 02:49:23 +0500

    bug: implement recursion into copy_static function insted shutil.copytree

--- src/copy_static.py
+++ src/copy_static.py
@@ -11,6 +11,20 @@ def clear_directory(path: str) -> None:
 
 
 def copy_static(src, dst):
-    clear_directory(dst)
-    shutil.copytree(src, dst, dirs_exist_ok=True)
-    print("Files was copied to docs!")
+    if os.path.exists(dst):
+        clear_directory(dst)
+    else:
+        os.mkdir(dst)
+
+    entrys = os.listdir(src)
+
+    for entry in entrys:
+        path_to_entry = os.path.join(src, entry)
+        if os.path.isfile(path_to_entry):
+            dest_path = os.path.join(dst, entry)
+            shutil.copy(path_to_entry, dest_path)
+        else:
+            new_dest_dir = os.path.join(dst, entry)
+            copy_static(path_to_entry, new_dest_dir)
+    # shutil.copytree(src, dst, dirs_exist_ok=True)
+    # print("Files was copied to docs!")
